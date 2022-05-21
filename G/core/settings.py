60d7c3a21e7abcf75dd3af2b655ff4251317b0c0
Andres Gomez <anmgomezmo@unal.edu.co>
2022-05-20 21:23:26 -0500

    feat: change project

--- G/core/settings.py
+++ G/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ylfucym-rc)1zgt5xr%lyn)2jtd(_yks=((%dizlukwgm#fj#'
+SECRET_KEY = 'django-insecure-(yfe8f(+!cnu_$(!neo6!e2)4hx5b&@^i6*i99*ph=@1b*yzzg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
 ]
 
 MIDDLEWARE = [
